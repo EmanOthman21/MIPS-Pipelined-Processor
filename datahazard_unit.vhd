af711acf7dd5fe536f22f743a34471a5ae99b977
bahaaEldeen1999 <bahaaeldeen1999@gmail.com>
2021-05-22 15:37:45 +0200

    add in2Mux should be done with datahazard unit

--- datahazard_unit.vhd
+++ datahazard_unit.vhd
@@ -3,7 +3,7 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 ENTITY DataHazardUnit IS
     PORT (
-        writeFlag : IN STD_LOGIC;
+        immFlag : IN STD_LOGIC;
         rsrc : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
         rdstMEMWB : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
         rdstEXMEM : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
@@ -15,8 +15,8 @@ ENTITY DataHazardUnit IS
 END DataHazardUnit;
 
 ARCHITECTURE DataHazardUnitDefault OF DataHazardUnit IS
-
-    PROCESS (writeFlag, rsrc, rdstMEMWB, rdstEXMEM, rdstWBSeclectorMEMWB, rdstWBSeclectorEXMEM)
+BEGIN
+    PROCESS (immFlag, rsrc, rdstMEMWB, rdstEXMEM, rdstWBSeclectorMEMWB, rdstWBSeclectorEXMEM)
     BEGIN
         IF (rdstMEMWB /= rsrc AND rdstEXMEM /= rsrc) OR (rdstWBSeclectorMEMWB = "00" AND rdstWBSeclectorEXMEM = "00") THEN
             in1Mux <= "00";
@@ -28,8 +28,23 @@ ARCHITECTURE DataHazardUnitDefault OF DataHazardUnit IS
         END IF;
 
         -- more readable approach for in2Mux
+        IF rdstMEMWB /= rsrc AND rdstEXMEM /= rsrc AND immFlag = '0' THEN
+            in2Mux <= "00";
+        ELSIF rdstMEMWB /= rsrc AND rdstEXMEM /= rsrc AND immFlag = '1' THEN
+            in2Mux <= "01";
+        ELSIF rdstWBSeclectorMEMWB = "00" AND rdstWBSeclectorEXMEM = "00" AND immFlag = '0' THEN
+            in2Mux <= "00";
+        ELSIF rdstWBSeclectorMEMWB = "00" AND rdstWBSeclectorEXMEM = "00" AND immFlag = '1' THEN
+            in2Mux <= "01";
+        ELSIF rdstMEMWB = rsrc AND rdstEXMEM /= rsrc AND rdstWBSeclectorMEMWB /= "00" THEN
+            in2Mux <= "10";
+        ELSIF rdstMEMWB /= rsrc AND rdstEXMEM = rsrc AND rdstWBSeclectorEXMEM /= "00" THEN
+            in2Mux <= "11";
+        ELSIF rdstMEMWB = rsrc AND rdstEXMEM = rsrc AND rdstWBSeclectorEXMEM = "00" AND rdstWBSeclectorMEMWB /= "00" THEN
+            in2Mux <= "10";
+        ELSIF rdstMEMWB = rsrc AND rdstEXMEM = rsrc AND rdstWBSeclectorEXMEM /= "00" AND rdstWBSeclectorMEMWB = "00" THEN
+            in2Mux <= "11";
+        END IF;
     END PROCESS;
-
-BEGIN
 
 END DataHazardUnitDefault; -- DataHazardUnitDefault
