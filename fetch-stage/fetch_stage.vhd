32a16d510374350f32fe68fbf63239e31493ba6a
EmanOthman21 <eothman21@gmail.com>
2021-06-04 01:12:38 +0200

    Fetch reset address

--- fetch-stage/fetch_stage.vhd
+++ fetch-stage/fetch_stage.vhd
@@ -14,17 +14,23 @@ END fetch;
 ARCHITECTURE fetch_stage OF fetch IS
 	SIGNAL stall_pc_mux_out : STD_LOGIC_VECTOR(31 DOWNTO 0);
 	SIGNAL pc_mux_out : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL pc_reset_mux_out : STD_LOGIC_VECTOR(31 DOWNTO 0);
 	SIGNAL pcMuxOut : STD_LOGIC_VECTOR(31 DOWNTO 0);
 
 	SIGNAL m0 : STD_LOGIC_VECTOR(31 DOWNTO 0);
 	SIGNAL irTemp : STD_LOGIC_VECTOR(31 DOWNTO 0);
 
 	SIGNAL pcAdder : STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL reset_falling : STD_LOGIC;
 BEGIN
+	reset_falling <= '1' when reset'EVENT AND reset='0'
+	else '0';
 
-	pc_mux : ENTITY work.mux_2_1 PORT MAP (reset, pcIn, m0, pc_mux_out);
+	pc_mux : ENTITY work.mux_2_1 PORT MAP (reset, pcIn, (others => '0'), pc_mux_out);
 
-	stall_pc_mux : ENTITY work.mux_2_1 PORT MAP (loadUse, pc_mux_out, stall_pc_mux_out, stall_pc_mux_out);
+	pc_memory_reset : ENTITY work.mux_2_1 PORT MAP (reset_falling,pc_mux_out,irTemp,pc_reset_mux_out);
+
+	stall_pc_mux : ENTITY work.mux_2_1 PORT MAP (loadUse, pc_reset_mux_out, stall_pc_mux_out, stall_pc_mux_out);
 
 	mainMemory : ENTITY work.instructions_memory GENERIC MAP (65000, 32, 16) PORT MAP (reset, stall_pc_mux_out, m0, irTemp);
 
